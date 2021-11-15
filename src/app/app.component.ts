729d93d7beb073004fbed2265d2ddd5c6d4a24f0
Fabián Cruz <fabiancruz3773@gmail.com>
2021-11-14 23:21:41 -0600

    custom cursor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 
 @Component({
@@ -6,15 +6,30 @@ import { Title } from '@angular/platform-browser';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit{
+  
+  ngOnInit(): void {
+
+    let cursor = document.getElementById('cursor')!;
+
+    document.addEventListener('mousemove', (e)=>{
+      cursor.style.left = e.pageX+"px";
+      cursor.style.top = e.pageY+"px";
+    })
+
+  }
 
   public constructor(private titleService: Title)
   {
      this.setTitle('Fabián Cruz')
    }
 
+   
+
   public setTitle(newTitle: string) {
     this.titleService.setTitle(newTitle);
   }
+
 
 }
